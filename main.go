3591b1a52aa309c19c497a96144d57b2b3b3c9a2
Tim Kennedy <t.m.knndy@gmail.com>
2015-04-07 22:27:52 +1000

    Added input in the form of spinning the player around.

--- main.go
+++ main.go
@@ -41,10 +41,30 @@ func main() {
 	window.MakeContextCurrent()
 
 	// game setup
+	player := obj.NewPlayer(Vec2{5,5})
+	window.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+		switch action {
+		case glfw.Press:
+			switch key {
+			case glfw.KeyLeft:
+				player.Physics.RotationalVelocity = -1
+			case glfw.KeyRight:
+				player.Physics.RotationalVelocity = 1
+			}
+		case glfw.Release:
+			switch key {
+			case glfw.KeyLeft:
+				player.Physics.RotationalVelocity = 0
+			case glfw.KeyRight:
+				player.Physics.RotationalVelocity = 0
+			}
+		}
+	})
+
 	objects := []obj.GameObject{
 		obj.NewAsteroid(12, Vec2{3, 3}, Vec2{2, 1}),
 		obj.NewAsteroid(5, Vec2{7, 7}, Vec2{1, 2}),
-		obj.NewPlayer(Vec2{5,5}),
+		player,
 	}
 	engine := eng.NewEngine(window, objects)
 
