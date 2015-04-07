8b594003e8bc7904724b6addbecdffe8c865005f
Tim Kennedy <t.m.knndy@gmail.com>
2015-04-07 22:41:40 +1000

    Moved more functionality into the engine.

--- eng/engine.go
+++ eng/engine.go
@@ -1,6 +1,9 @@
 package eng
 
 import (
+	"log"
+	"runtime"
+
 	"github.com/go-gl/gl/v4.5-core/gl"
 	. "github.com/go-gl/mathgl/mgl32"
 	"github.com/go-gl/glfw/v3.1/glfw"
@@ -10,15 +13,22 @@ import (
 )
 
 type Engine struct {
-	objects []obj.GameObject
-	Window *glfw.Window
+	Objects []obj.GameObject
+	*glfw.Window
 }
 
-func NewEngine(window *glfw.Window, objects []obj.GameObject) *Engine {
-	return &Engine{objects: objects, Window: window}
+func init() {
+	runtime.LockOSThread() 		           // switching GoRoutines between threads will break OpenGL
+}
+
+func NewEngine(winHeight, winWidth int) *Engine {
+	window := NewWindow(winWidth, winHeight)
+	window.MakeContextCurrent()
+	return &Engine{Window: window}
 }
 
 func (eng *Engine) Start() {
+	defer glfwTeardown()
 	previousTime := glfw.GetTime()
 	for !eng.Window.ShouldClose() {
 		//// SETUP
@@ -38,12 +48,12 @@ func (eng *Engine) Start() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		// handle physics, controls, etc.
-		for _, o := range eng.objects {
+		for _, o := range eng.Objects {
 			o.Update(height, width, elapsed)
 		}
 
 		// draw to window
-		for _, o := range eng.objects {
+		for _, o := range eng.Objects {
 			o.Render(vp)
 		}
 
@@ -52,4 +62,37 @@ func (eng *Engine) Start() {
 		eng.Window.SwapBuffers()
 		glfw.PollEvents()
 	}
+}
+
+func glSetup() {
+	err := gl.Init()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	gl.Enable(gl.DEPTH_TEST) // depth testing
+	gl.DepthFunc(gl.LESS)    // smaller is closer
+}
+
+func glfwSetup()  {
+	err := glfw.Init()
+	if err != nil {
+		log.Panic(err)
+	}
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+}
+
+func glfwTeardown() {
+	glfw.Terminate()
+}
+
+func NewWindow(h int, w int) *glfw.Window {
+	glSetup()
+	glfwSetup()
+	window, err := glfw.CreateWindow(h, w, "Gasteroids", glfw.GetPrimaryMonitor(), nil)
+	window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+	if err != nil {
+		log.Panic(err)
+	}
+	return window
 }
--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"log"
-	"runtime"
 	"math/rand"
 	"time"
 	"os"
 	"strconv"
 
-	"github.com/go-gl/gl/v4.5-core/gl"
 	. "github.com/go-gl/mathgl/mgl32"
 	"github.com/go-gl/glfw/v3.1/glfw"
 
@@ -17,7 +14,6 @@ import (
 )
 
 func init() {
-	runtime.LockOSThread() 		           // switching GoRoutines between threads will break OpenGL
 	rand.Seed(time.Now().UTC().UnixNano()) // make random behave unpredictably
 }
 
@@ -32,17 +28,11 @@ func main() {
 		winWidth = 640
 	}
 
-	//// SETUP
-	// gl/glfw setup
-	glSetup()
-	glfwSetup()
-	defer glfwTeardown()
-	window := NewWindow(winWidth, winHeight)
-	window.MakeContextCurrent()
+	engine := eng.NewEngine(winHeight, winWidth)
 
-	// game setup
+	//// game setup
 	player := obj.NewPlayer(Vec2{5,5})
-	window.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	engine.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
 		switch action {
 		case glfw.Press:
 			switch key {
@@ -61,44 +51,12 @@ func main() {
 		}
 	})
 
-	objects := []obj.GameObject{
+	engine.Objects = []obj.GameObject{
 		obj.NewAsteroid(12, Vec2{3, 3}, Vec2{2, 1}),
 		obj.NewAsteroid(5, Vec2{7, 7}, Vec2{1, 2}),
 		player,
 	}
-	engine := eng.NewEngine(window, objects)
 
 	//// MAIN LOOP
 	engine.Start()
-}
-
-func glSetup() {
-	err := gl.Init()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	gl.Enable(gl.DEPTH_TEST) // depth testing
-	gl.DepthFunc(gl.LESS)    // smaller is closer
-}
-
-func glfwSetup()  {
-	err := glfw.Init()
-	if err != nil {
-		log.Panic(err)
-	}
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-}
-
-func glfwTeardown() {
-	glfw.Terminate()
-}
-
-func NewWindow(h int, w int) *glfw.Window {
-	window, err := glfw.CreateWindow(h, w, "Gasteroids", glfw.GetPrimaryMonitor(), nil)
-	window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
-	if err != nil {
-		log.Panic(err)
-	}
-	return window
 }
